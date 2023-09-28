3915636f43a388e3e0f5d158b1f2243bfa56e173
Fatih Dogan <fatih.dogan@bitlc.onmicrosoft.com>
2023-09-28 11:55:56 +0200

    Update

---
+++ Aufgabe_Array_Min_Max/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.SymbolStore;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aufgabe_Array_Min_Max
+{
+    internal class Program
+    {
+        delegate bool VergleichsDelegate(int x, int y);
+        static void Main(string[] args)
+        {
+
+           
+
+        }
+    }
+}
